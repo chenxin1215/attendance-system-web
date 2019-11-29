02baa24732a8e8b513230572478b9acfd8653594
xx <duqian0910@163.com>
2019-11-30 00:39:01 +0800

    fix

--- js/Ajax.js
+++ js/Ajax.js
@@ -3,7 +3,7 @@ $.ajax({
 	url:"",  // 请求地址
 	async:false,
 	dataType : "json",
-	data:{   // 发送的数据
+	data:{   // 发送的数据asdasd
 		
 	},
 	success:function(message){  //成功后执行的函数  message为后台返回的数据集 json格式
