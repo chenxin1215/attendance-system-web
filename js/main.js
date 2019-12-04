2f66f8a41c29dbdfbcaacb4ea9c0d0345863906b
chenxin1215 <154660165@qq.com>
2019-12-05 00:24:03 +0800

    1.个人审批信息查看界面 2.个人加班记录界面 3.个人请假记录界面

--- js/main.js
+++ js/main.js
@@ -6,7 +6,15 @@ $(function(){
 	$(".attendance_info_button").click(function () {
 		$("#frame").attr("src","attendance_info.html");
 	});
-	
+	$(".overtime_button").click(function () {
+		$("#frame").attr("src","overtime_info.html");
+	});
+	$(".leave_button").click(function () {
+		$("#frame").attr("src","leave_info.html");
+	});
+	$(".approve_button").click(function () {
+		$("#frame").attr("src","approve_info.html");
+	});
 	
 	
 });
