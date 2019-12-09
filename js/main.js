97b6bf099cb54e74cbe7ba5cde711ee4e74c921a
xx <duqian0910@qq.com>
2019-12-09 22:37:15 +0800

    。。。

--- js/main.js
+++ js/main.js
@@ -18,5 +18,16 @@ $(function(){
 	$(".user-name").click(function(){
 		$("#frame").attr("src","person_info.html")
 	});
-	
+	$(".attendance_rule_configure").click(function(){
+		$("#frame").attr("src","attendance_rule_configure.html")
+	});
+	$(".attendance_info_manage").click(function(){
+		$("#frame").attr("src","attendance_manage.html")
+	});
+	$(".approve_info_manage").click(function(){
+		$("#frame").attr("src","approve_info_manage.html")
+	});
+	$(".employee_manage").click(function(){
+		$("#frame").attr("src","employee_manage.html")
+	});
 });
