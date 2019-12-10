8142ef1225f1b2cd3c7a01d6e6ce4f1cf6e15f9f
xx <duqian0910@qq.com>
2019-12-10 18:34:42 +0800

    事件

--- js/Ajax.js
+++ js/Ajax.js
@@ -1,15 +1,17 @@
-$.ajax({
-	type:"post",
-	url:"",  // 请求地址
-	async:false,
-	dataType : "json",
-	data:{   // 发送的数据asdasd
-		
-	},
-	success:function(message){  //成功后执行的函数  message为后台返回的数据集 json格式
-		
-	},
-	error : function(){ //错误后执行的函数
-		
-	}
+$(function() {
+	$.ajax({
+		type: "post",
+		url: "", // 请求地址
+		async: false,
+		dataType: "json",
+		data: { // 发送的数据asdasd
+
+		},
+		success: function(message) { //成功后执行的函数  message为后台返回的数据集 json格式
+
+		},
+		error: function() { //错误后执行的函数
+
+		}
+	});
 });
--- js/index.js
+++ js/index.js
@@ -1,13 +1,41 @@
-$(function () {
-	$(".up-bt").click(function(){
-		$(this).html("打卡时间：8:35:54");
-		$(this).addClass("uped-font");
-		$(this).addClass("disabled");
+$(function() {
+	$(".up-bt").click(function() {
+//		$(this).html("打卡时间：8:35:54");
+//		$(this).addClass("uped-font");
+//		$(this).addClass("disabled");
+		$.ajax({
+		type: "post",
+		url: "attendance/punchIn", // 请求地址
+		async: false,
+		dataType: "json",
+		success: function(message) {
+			//成功后执行的函数  message为后台返回的数据集 json格式
+			if(message.rspCode == 1) {
+
+			} else if(message.rspCode == -1) {
+				alert(message.rspMsg);
+			}
+		}
 	});
-	
-	$(".down-bt").click(function(){
-		$(this).html("打卡时间：18:35:54");
-		$(this).addClass("uped-font");
-		$(this).addClass("disabled");
+	});
+
+	$(".down-bt").click(function() {
+//		$(this).html("打卡时间：18:35:54");
+//		$(this).addClass("uped-font");
+//		$(this).addClass("disabled");
+		$.ajax({
+		type: "post",
+		url: "attendance/punchOut", // 请求地址
+		async: false,
+		dataType: "json",
+		success: function(message) {
+			//成功后执行的函数  message为后台返回的数据集 json格式
+			if(message.rspCode == 1) {
+
+			} else if(message.rspCode == -1) {
+				alert(message.rspMsg);
+			}
+		}
+	});
 	});
 });
